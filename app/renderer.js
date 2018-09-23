7bcb9a53b269f6e90e4f4b1e9ca2ba3e6528cfe1
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-22 21:36:11 -0700

    totally rewrote main.js, cleaned up styles and index.html

--- app/renderer.js
+++ app/renderer.js
@@ -1,11 +1,11 @@
 const { remote, ipcRenderer } = require('electron');
 const mainProcess = remote.require('./main');
 
-const openFileButton = document.querySelector('#open-file');
+// const openFileButton = document.querySelector('#open-file');
 
-let filePath = null;
-let originalContent = '';
+// let filePath = null;
+// let originalContent = '';
 
-openFileButton.addEventListener('click', () => {
-  mainProcess.openFile();
-});
+// openFileButton.addEventListener('click', () => {
+//   mainProcess.openFile();
+// });
