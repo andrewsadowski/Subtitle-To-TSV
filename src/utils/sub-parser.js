29f3c619fb039895a4466a7b7ed47728451417d1
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-25 19:40:36 -0700

    added JSDOC block comments for all parameters in sub-parser.js

--- src/utils/sub-parser.js
+++ src/utils/sub-parser.js
@@ -2,6 +2,12 @@ const parser = require('subtitles-parser');
 const fs = require('fs');
 const path = require('path');
 
+/**
+ *
+ * @param {string} file - Accepts a file-path
+ * @return {array} subArr - Returns an array of tabulated subtitle data
+ */
+
 const subParser = file => {
   let srt = fs.readFileSync(file, 'utf8');
   let data = parser.fromSrt(srt);
@@ -14,6 +20,14 @@ const subParser = file => {
   });
   return subArr;
 };
+
+/**
+ *
+ * @param {array} contentArr - Array of tabulated subtitle data
+ * @param {string} outputPath - Path to output directory
+ * @param {string} fileName - Name of file without extension/path
+ */
+
 const generateTSV = (contentArr, outputPath, fileName) => {
   let content = contentArr.join('\n');
   let pathForOutput = path.join(outputPath + '/' + fileName + '.tsv');
