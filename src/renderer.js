40f2afbac59f87a9947396562408e818affb3fba
admin <andrew.n.sadowski@gmail.com>
2018-10-18 15:25:48 -0700

    added isDirectory check to determine if path from user is an individual file or a directory

--- src/renderer.js
+++ src/renderer.js
@@ -1,16 +1,16 @@
-const { remote, ipcRenderer } = require('electron');
-const { dialog } = require('electron').remote;
-const mainProcess = remote.require('./main');
+const { remote, ipcRenderer } = require("electron");
+const { dialog } = require("electron").remote;
+const mainProcess = remote.require("./main");
 const currentWindow = remote.getCurrentWindow();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const { subParser, generateTSV } = require('./utils/sub-parser');
+const { subParser, generateTSV } = require("./utils/sub-parser");
 
-const chooseFile = document.querySelector('#choose-file');
-const saveTo = document.querySelector('#save-file');
-const execute = document.querySelector('#execute');
-const dragContainer = document.querySelector('#drag-container');
+const chooseFile = document.querySelector("#choose-file");
+const saveTo = document.querySelector("#save-file");
+const execute = document.querySelector("#execute");
+const dragContainer = document.querySelector("#drag-container");
 
 let filePathForSub = null;
 let dirPathForOutput = null;
@@ -30,20 +30,33 @@ const getDefaultDirPath = filePath => {
 };
 
 /**
+ *
+ * @param {string} pathFromUser - absolute path from user
+ * @return {bool} - returns true if is a directory or false if ext is present
+ */
+const isDirectory = pathFromUser => {
+  if (path.extname(pathFromUser) === "") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
  * * ChooseFile button event listener
  * *  - updates filePathForSub variable with path info
  * *  - onClick adds 'completed' class to DOM element
  */
 
-chooseFile.addEventListener('click', e => {
+chooseFile.addEventListener("click", e => {
   const files = dialog.showOpenDialog({
-    properties: ['openFile']
+    properties: ["openFile"]
   });
   filePathForSub = files[0];
-  subtitleFileName = path.basename(filePathForSub, '.srt');
+  subtitleFileName = path.basename(filePathForSub, ".srt");
   console.log(files);
-  chooseFile.classList.add('completed');
-  execute.classList.add('ready');
+  chooseFile.classList.add("completed");
+  execute.classList.add("ready");
 });
 
 /**
@@ -52,16 +65,16 @@ chooseFile.addEventListener('click', e => {
  * * -onClick adds 'completed' class to DOM element
  */
 
-saveTo.addEventListener('click', e => {
+saveTo.addEventListener("click", e => {
   const directoryOfChoice = dialog.showOpenDialog({
-    properties: ['openDirectory']
+    properties: ["openDirectory"]
   });
   dirPathForOutput = directoryOfChoice[0];
   console.log(`dirPathForOutput:${dirPathForOutput}`);
-  saveTo.classList.add('completed');
+  saveTo.classList.add("completed");
 });
 
-execute.addEventListener('click', e => {
+execute.addEventListener("click", e => {
   if ((filePathForSub && dirPathForOutput) || defaultDirPath) {
     generateTSV(
       subParser(filePathForSub),
@@ -69,19 +82,18 @@ execute.addEventListener('click', e => {
       subtitleFileName
     );
     alert(
-      `Your file has been created at: ${dirPathForOutput ||
-        defaultDirPath}`
+      `Your file has been created at: ${dirPathForOutput || defaultDirPath}`
     );
   }
   if (!filePathForSub) {
-    alert('Please provide a subtitle.');
+    alert("Please provide a subtitle.");
   }
   if (!dirPathForOutput && !defaultDirPath) {
-    alert('Please provide a directory to output to.');
+    alert("Please provide a directory to output to.");
   }
-  saveTo.classList.remove('completed');
-  chooseFile.classList.remove('completed');
-  execute.classList.remove('ready');
+  saveTo.classList.remove("completed");
+  chooseFile.classList.remove("completed");
+  execute.classList.remove("ready");
 });
 
 /**
@@ -92,7 +104,7 @@ execute.addEventListener('click', e => {
  */
 
 document.addEventListener(
-  'dragover',
+  "dragover",
   e => {
     e.preventDefault();
     e.stopPropagation();
@@ -101,35 +113,35 @@ document.addEventListener(
   false
 );
 
-document.addEventListener('ondragstart', e => {
+document.addEventListener("ondragstart", e => {
   e.preventDefault();
   e.stopPropagation();
   return false;
 });
 
-document.addEventListener('ondragleave', e => {
+document.addEventListener("ondragleave", e => {
   e.preventDefault();
   e.stopPropagation();
   return false;
 });
 
-document.addEventListener('ondragend', e => {
+document.addEventListener("ondragend", e => {
   e.preventDefault();
   e.stopPropagation();
   return false;
 });
 
 document.addEventListener(
-  'drop',
+  "drop",
   e => {
     e.preventDefault();
     filePathForSub = e.dataTransfer.files[0].path;
     getDefaultDirPath(filePathForSub);
-    subtitleFileName = path.basename(filePathForSub, '.srt');
+    subtitleFileName = path.basename(filePathForSub, ".srt");
     console.log(
       `FilePathForSub${filePathForSub} \n subtitleFileName:${subtitleFileName}\ndefaultDirPath: ${defaultDirPath}`
     );
-    execute.classList.add('ready');
+    execute.classList.add("ready");
     // dragContainer.classList.add('readyWithText');
     return false;
   },
